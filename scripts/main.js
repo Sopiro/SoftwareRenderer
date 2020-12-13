87654688bde506bbbd087b48d66a5f74299e86b7
Sopiro <knwer782@naver.com>
2020-12-13 20:05:33 +0900

    Base

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,66 @@
-function main()
+let WIDTH = 800;
+let HEIGHT = 600;
+let SCALE = 4;
+
+let d = new Date();
+let previousTime = 0;
+let currentTime = 0;
+let passedTime = 0;
+let msPerFrame = 1000.0 / 144.0;
+
+let imageData;
+
+let cvs;
+let gfx;
+
+function start()
 {
-    
+    init();
+    run();
 }
 
-window.onload = main();
+function init()
+{
+    cvs = document.getElementById("canvas");
+    cvs.setAttribute("width", WIDTH + "px");
+    cvs.setAttribute("height", HEIGHT + "px");
+    gfx = cvs.getContext("2d");
+
+    WIDTH = WIDTH / SCALE;
+    HEIGHT = HEIGHT / SCALE;
+
+    imageData = new ImageData(WIDTH, HEIGHT);
+
+    previousTime = new Date().getTime();
+}
+
+function run(time)
+{
+    let currentTime = new Date().getTime();
+    passedTime += currentTime - previousTime;
+    previousTime = currentTime;
+
+    while (passedTime >= msPerFrame)
+    {
+        update(msPerFrame);
+        render();
+        passedTime -= msPerFrame;
+    }
+
+    requestAnimationFrame(run);
+}
+
+function update(msPerFrame)
+{
+    for (let index = 0; index < imageData.data.length; index++)
+    {
+        imageData.data[index] = Math.random() * 255;
+    }
+}
+
+function render()
+{
+    gfx.putImageData(imageData, 0, 0);
+}
+
+window.onload = start;

43bbefbd116cc58a03c0f190f978e539b9c2cef3
sopiro <sopiro.ldh@gmail.com>
2023-10-29 01:28:05 +0900

    Convert mesh into a more memory efficient representation.

--- scripts/mesh.js
+++ scripts/mesh.js
@@ -6,44 +6,30 @@ export class Mesh
 {
     constructor(positions, texCoords, normals, indices)
     {
-        this.positions = positions;
-        this.texCoords = texCoords;
-        this.normals = normals;
-        this.indices = indices;
         this.faces = [];
 
-        for (let i = 0; i < this.indices.length; ++i)
+        for (let i = 0; i < indices.length; ++i)
         {
-            let vFace = this.indices[i];
+            let vFace = indices[i];
 
-            let face = [];
+            let vertices = [];
             for (let v = 0; v < 3; v++)
             {
-                const pos = this.getPosition(vFace[v][0] - 1);
-                const tex = this.getTexCoord(vFace[v][1] - 1);
-                const nor = this.getNormal(vFace[v][2] - 1);
-                face.push(new Vertex(pos, 0xffffff, tex, nor));
+                const p = vFace[v][0] - 1;
+                const t = vFace[v][1] - 1;
+                const n = vFace[v][2] - 1;
+
+                const pos = new Vec3(positions[p][0], positions[p][1], positions[p][2]);
+                const tex = new Vec2(texCoords[t][0], texCoords[t][1]);
+                const nor = new Vec3(normals[n][0], normals[n][1], normals[n][2]);
+
+                vertices.push(new Vertex(pos, 0xffffff, tex, nor));
             }
 
-            face = new Face(face[0], face[1], face[2]);
+            let face = new Face(vertices[0], vertices[1], vertices[2]);
             face.calcTangentAndBiTangent();
 
             this.faces.push(face);
         }
-    }
-
-    getPosition(pos)
-    {
-        return new Vec3(this.positions[pos][0], this.positions[pos][1], this.positions[pos][2]);
-    }
-
-    getTexCoord(tex)
-    {
-        return new Vec2(this.texCoords[tex][0], this.texCoords[tex][1]);
-    }
-
-    getNormal(nor)
-    {
-        return new Vec3(this.normals[nor][0], this.normals[nor][1], this.normals[nor][2]);
     }
 }
